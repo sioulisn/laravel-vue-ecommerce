751b5ee3430bae0b8401f432ad0f0141387a2e05
Sioulis Nick <sioulisn@gmail.com>
2023-04-09 20:50:49 +0300

    Create request password reset and reset password pages

--- backend/src/views/ResetPassword.vue
+++ backend/src/views/ResetPassword.vue
@@ -3,43 +3,35 @@
     <div class="flex min-h-full items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
         <div class="w-full max-w-md space-y-8">
             <div>
-                <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your
-                    account</h2>
+                <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Set new password</h2>
 
             </div>
             <form action="#" class="mt-8 space-y-6" method="POST">
                 <input name="remember" type="hidden" value="true"/>
                 <div class="-space-y-px rounded-md shadow-sm">
                     <div>
-                        <label class="sr-only" for="email-address">Email address</label>
-                        <input id="email-address" autocomplete="email"
+                        <label class="sr-only" for="new-password">New password</label>
+                        <input id="new-password"
                                class="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                               name="email" placeholder="Email address"
+                               name="email" placeholder="New Password"
                                required=""
-                               type="email"/>
+                               type="password"/>
                     </div>
                     <div>
-                        <label class="sr-only" for="password">Password</label>
-                        <input id="password" autocomplete="current-password"
+                        <label class="sr-only" for="password">Repeat Password</label>
+                        <input id="password-repeat"
                                class="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                               name="password" placeholder="Password"
+                               name="password-repeat" placeholder="Repeat Password"
                                required=""
                                type="password"/>
                     </div>
                 </div>
 
                 <div class="flex items-center justify-between">
-                    <div class="flex items-center">
-                        <input id="remember-me"
-                               class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                               name="remember-me"
-                               type="checkbox"/>
-                        <label class="ml-2 block text-sm text-gray-900" for="remember-me">Remember me</label>
-                    </div>
 
                     <div class="text-sm">
-                        <router-link :to="{name: 'requestPassword'}"
-                                     class="font-medium text-indigo-600 hover:text-indigo-500">Forgot your password?
+                        <router-link :to="{name: 'login'}" class="font-medium text-indigo-600 hover:text-indigo-500">
+                            Go back to Login
                         </router-link>
                     </div>
                 </div>
@@ -51,7 +43,7 @@
             <span class="absolute inset-y-0 left-0 flex items-center pl-3">
               <LockClosedIcon aria-hidden="true" class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"/>
             </span>
-                        Sign in
+                        Submit
                     </button>
                 </div>
             </form>
