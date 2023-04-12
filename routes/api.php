62626c4ae4d1bd949bd9764c69fe63aeeec18963
Sioulis Nick <sioulisn@gmail.com>
2023-04-12 20:44:45 +0300

    Implement Product CRUD in Laravel API

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum', 'admin'])->group(function () {
     Route::get('/user', [\App\Http\Controllers\AuthController::class, 'getUser']);
     Route::post('/logout', [\App\Http\Controllers\AuthController::class, 'logout']);
+
+    Route::apiResource('product', \App\Http\Controllers\ProductController::class);
 });
 
 Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
